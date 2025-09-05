313a349742f1bbf74e7516227f89872b8bd2410f
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2025-09-05 13:21:40 +0200

    feat: adapt connection to postgre

--- src/utils/sequelize.js
+++ src/utils/sequelize.js
@@ -1,15 +1,24 @@
 const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.MYSQL_URL, {
-  logging: false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+    logging: false,
+  }
+);
 
 (async () => {
   try {
     await sequelize.authenticate();
-    await sequelize.sync({
-      alter: true,
-    });
+    // await sequelize.sync({
+    //   alter: true,
+    // });
     console.log("Connection has been established successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
