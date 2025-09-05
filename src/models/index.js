c1daf231829915a5c736345402a856a0efd6930e
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2025-09-05 11:36:20 +0200

    fix: clean ald relations

--- src/models/index.js
+++ src/models/index.js
@@ -1,10 +1,1 @@
 const User = require('./User')
-const Restaurant = require('./Restaurant');
-const Dish = require('./Dish');
-
-// relations between models
-User.hasOne(Restaurant, { foreignKey: "ownerId" });
-Restaurant.belongsTo(User, { foreignKey: "ownerId" });
-
-Restaurant.hasMany(Dish, { foreignKey: "restaurantId" });
-Dish.belongsTo(Restaurant, { foreignKey: "restaurantId" });
