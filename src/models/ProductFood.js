f6139d8c9db72c2056174d2b6591b643aad5d038
Anastasiia A. <95031679+anastasiiaal@users.noreply.github.com>
2025-10-27 11:45:30 +0100

    feat: add ProductFood table end

--- src/models/ProductFood.js
+++ src/models/ProductFood.js
@@ -51,7 +51,28 @@ const ProductFood = sequelize.define(
             type: DataTypes.DECIMAL(5,2),
             allowNull: true,
         },
+        scores: {
+            type: DataTypes.JSONB,
+            allowNull: true,
+            defaultValue: null,
+            comment: "JSON object containing various score metrics for the product food",
+        },
+        analyzed_at: {
+            type: DataTypes.DATE,
+            allowNull: true,
+            defaultValue: null,
+        },
         fediaf_conformity: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+            defaultValue: false,
+        },
+        has_chemical_additives: {
+            type: DataTypes.BOOLEAN,
+            allowNull: true,
+            defaultValue: false,
+        },
+        has_beneficial_additives: {
             type: DataTypes.BOOLEAN,
             allowNull: true,
             defaultValue: false,
@@ -60,6 +81,11 @@ const ProductFood = sequelize.define(
             type: DataTypes.TEXT,
             allowNull: true,
             comment: "Source(s) of product information for analysis",
+        },
+        score_version: {
+            type: DataTypes.TEXT,
+            allowNull: true,
+            comment: "Version of the scoring algorithm used for analysis",
         },
     },
     {
